fdb206eef27c1f2b54afa571e74ab8f8a8b938fe
Jonathan-Nasser <virotico@gmail.com>
2024-07-26 19:11:05 -0300

    primeiro repositorio

---
+++ script.js
@@ -0,0 +1,12 @@
+let formulario = document.querySelector(".fale-conosco")
+let mascara = document.querySelector (".mascara-form")
+
+
+function chamarForm() {
+    formulario.style.left = "700px"
+    mascara.style.visibility = "visible"
+}
+
+function sumirForm() {
+    formulario.style.left = "-320px"
+}
